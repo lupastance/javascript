72bf95ab76e1ca95a09343f37b87b1149242c0c3
Lupa Stance <genzokun@gmail.com>
2024-10-18 22:50:18 +0200

    Editar y borrar terminadas

--- ola/script/main.js
+++ ola/script/main.js
@@ -4,24 +4,30 @@ const tareas = document.querySelector('#tareas');
 
 btnNueva.addEventListener('click', ()=> {
     const nuevaTarea = document.createElement('li');
+    const nuevaTareaTitulo = document.createElement('span');
     const nuevaTareaBorrar = document.createElement('button');
     const nuevaTareaEditar = document.createElement('button');
 
-    nuevaTarea.innerText = txtNueva.value;
+    nuevaTareaTitulo.innerText = txtNueva.value;
     nuevaTareaBorrar.innerText = '❌';
     nuevaTareaEditar.innerText = '✏️';
 
     nuevaTareaBorrar.addEventListener('click', () => {
-        console.log('Borrar Tarea');
+        console.log('❌ Borrando la Tarea ⚠️', nuevaTareaTitulo.innerText);
         tareas.removeChild(nuevaTarea);
     });
 
     nuevaTareaEditar.addEventListener('click', () => {
-        console.log('Editar Tarea');
+        console.log('✏️ Editando la tarea 👉🏻', nuevaTarea.innerText);
+
+        const tareaEditada = prompt('Cambia el nombre de la tarea', nuevaTareaTitulo.innerText);
+        nuevaTareaTitulo.innerText = tareaEditada;        
     });
     
+    nuevaTarea.appendChild(nuevaTareaTitulo);
     nuevaTarea.appendChild(nuevaTareaBorrar);
     nuevaTarea.appendChild(nuevaTareaEditar);
+
     tareas.appendChild(nuevaTarea);
     
     txtNueva.value = '';
